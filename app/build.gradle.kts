fc0fffcb471a26ad5655fb71cb96cd5ec678f8e4
kayolik <kieta2137@gmail.com>
2026-06-05 02:35:58 +0200

    v1.9.0: drop Status card from GUI

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -10,8 +10,8 @@ android {
         applicationId = "com.example.kayolikAOD"
         minSdk = 31
         targetSdk = 35
-        versionCode = 1
-        versionName = "1.8.1"
+        versionCode = 2
+        versionName = "1.9.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
